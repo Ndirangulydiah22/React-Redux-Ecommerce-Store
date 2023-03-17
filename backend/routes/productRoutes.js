9f502d01303db57312a0136d4ffe63a0f1b49611
unknown <FRANCIS@KE-LPT-B368.KEBETIKA.local>
2023-03-17 09:39:35 +0300

    Ready for deployment

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getProducts,
+  getProductsById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+} from "../controllers/productController.js";
+
+import { admin, protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductsById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+
+export default router;
